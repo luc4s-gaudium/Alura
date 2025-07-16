5875b0c5a366023975baba64b622692df8c257ec
Lucas Silva <lucassilva@gaudium.com.br>
2025-07-16 16:23:13 -0300

    trabalhando com arrays

---
+++ avancando-com-php-arrays-strings-funcao-e-web/avancando/listas.php
@@ -0,0 +1,5 @@
+<?php
+$idades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$primeiraIdade = $idades[0];
+
+echo $primeiraIdade;
---
+++ avancando-com-php-arrays-strings-funcao-e-web/avancando/loop-listas.php
@@ -0,0 +1,6 @@
+<?php
+$idades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for ($i = 0; $i < count($idades); $i++) {
+    echo "$idades[$i]" . PHP_EOL;
+}
